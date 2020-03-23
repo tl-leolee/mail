c16f1ebc1262a589106b8e9f09365f0737100af6
Leo <tl.leolee@gmail.com>
2020-03-23 09:48:40 -0400

    -j2

--- mail.js
+++ mail.js
@@ -1,7 +1,7 @@
 var sendmail = require('sendmail')()
 
 
-module.exports = async function send({ from='no-reply@leoleesite.com', to='tl.leolee@outlook.com', subject='sendmail test email', html='<html><p>Hello world.</p></html>' }) {
+module.exports = async function send(options = { from='no-reply@leoleesite.com', to='tl.leolee@outlook.com', subject='sendmail test email', html='<html><p>Hello world.</p></html>' }) {
   sendmail({
     from,
     to,
