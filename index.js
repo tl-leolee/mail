52c58d7f605fedcb79eac2034c9150f723d8de72
Leo <tl.leolee@gmail.com>
2020-03-23 09:47:35 -0400

    -j2

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ var send = require('./mail')
 const PORT = process.env.PORT || 3000
 
 app.get('/send', async (req, res) => {
-
   await send();
-
   res.sendStatus(200)
-
 })
 
 app.listen(PORT, () => {
