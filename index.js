1f77240148580ad60f471fc62abae2dddcbc123e
Leo <tl.leolee@gmail.com>
2020-03-23 08:20:59 -0400

    test

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+var sendmail = require('sendmail')()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/send', async (req, res) => {
+
+  await sendmail({
+    from: 'no-reply@leoleesite.com',
+    to: 'tl.leolee@outlook.com',
+    subject: 'MailComposer sendmail',
+    html: 'Mail of test sendmail '
+    }, function (err, reply) {
+    console.log(err && err.stack)
+    console.dir(reply)
+  })
+
+  res.sendStatus(200)
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
