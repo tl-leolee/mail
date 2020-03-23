1fc255ff364b5cb0c0c04297f658fef96e1e1699
Leo <tl.leolee@gmail.com>
2020-03-23 09:53:34 -0400

    -j2

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ var sendmail = require('sendmail')()
 var defaultOpt = { 
   from:'no-reply@leoleesite.com', 
   to:'tl.leolee@gmail.com', 
-  subject:'sendmail test email', 
-  html:'<html><p>Hello world.</p></html>' 
+  subject:'MailComposer sendmail', 
+  html:'<html><p>Mail of test sendmail</p></html>' 
 }
 
 module.exports = async function send(options=defaultOpt) {
