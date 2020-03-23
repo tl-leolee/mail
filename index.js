040f67502f9ca1948627afe3017e26fb654f85a4
Leo <tl.leolee@gmail.com>
2020-03-23 09:46:18 -0400

    j

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 var express = require('express');
 
 var app = express();
-var sendmail = require('sendmail')()
+
+var send = require('./mail')
+
 
 const PORT = process.env.PORT || 3000
 
 app.get('/send', async (req, res) => {
 
-  await sendmail({
-    from: 'no-reply@leoleesite.com',
-    to: 'tl.leolee@outlook.com',
-    subject: 'MailComposer sendmail',
-    html: 'Mail of test sendmail '
-    }, function (err, reply) {
-    console.log(err && err.stack)
-    console.dir(reply)
-  })
+  await send();
 
   res.sendStatus(200)
 
---
+++ mail.js
@@ -0,0 +1,14 @@
+var sendmail = require('sendmail')()
+
+
+module.export = async function send({ from='no-reply@leoleesite.com', to='tl.leolee@outlook.com', subject='sendmail test email', html='<html><p>Hello world.</p></html>' }) {
+  sendmail({
+    from,
+    to,
+    subject,
+    html,
+  }, function (err, reply) {
+    console.log(err && err.stack)
+    console.dir(reply)
+  })
+}
